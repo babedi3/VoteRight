05236a14fe884c92304fcf318678ac442a375812
Brian Abedi <babedi3@gatech.edu>
2020-11-08 20:40:09 -0500

    AWS Amplify for added Login/Signup page

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {
   StatusBar, 
-  Image
+  Image,
 } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import UnitedStates from './screens/UnitedStates';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { withAuthenticator } from 'aws-amplify-react-native';
 
 
 const Tab = createBottomTabNavigator();
@@ -47,6 +48,66 @@ const App: () => React$Node = () => {
       </Tab.Navigator>
     </NavigationContainer>
   );
-};
+}; 
+/* 
+class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <StatusBar barStyle="dark-content"/>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              if (route.name === 'Settings') {
+                return <Ionicons name="settings" size={size} color={'black'}/>;
+              }
+              if (route.name === 'State') {
+                return <Image source={require('./assets/georgia.png')}/>;
+              }
+              if (route.name === 'US') {
+                return <Image source={require('./assets/us.png')}/>;
+              }
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'white',
+            inactiveTintColor: 'white',
+            showLabel: 'false',
+          }}
+        > 
+          <Tab.Screen name="State" component={HomeScreen} />
+          <Tab.Screen name="US" component={UnitedStates} />
+          <Tab.Screen name="Settings" component={HomeScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
 
-export default App;
+class MySectionHeader extends sectionHeader {
+  render() {
+    return (
+      <View style={styles.container}>
+        <img style={{width: 250, height: 250}}
+        src={logo}/>
+      </View>
+    )}
+};
+/* class theme extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <img style={{width: 250, height: 250}}
+        src={logo}/>
+      </View>
+    );
+}}; 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
+ */
+
+export default withAuthenticator(App, true);
