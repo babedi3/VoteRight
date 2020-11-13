043363727c708a69f88fd814cacedf3024c08ff0
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-11-13 04:44:09 +0400

    redux and api backend

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { withAuthenticator } from 'aws-amplify-react-native';
+import { Provider } from "react-redux";
+import { store } from "./redux/store"
 
 import Router from './router'
 
 const App: () => React$Node = () => {
   return (
+    <Provider store={store}>
     <NavigationContainer>
       <StatusBar barStyle="dark-content"/>
       <Router/>
     </NavigationContainer>
+    </Provider>
     
   );
 }; 
